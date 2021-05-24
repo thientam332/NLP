3d82f3f62b516c5388505904580732dfb0abc307
thientam332 <76957177+thientam332@users.noreply.github.com>
2021-05-25 01:22:35 +0700

    add docstring

--- dataset/embeddings.py
+++ dataset/embeddings.py
@@ -6,21 +6,71 @@ class Embeddings():
     """
 
     def __init__(self, path, vector_dimension):
+        """
+        
+
+        Parameters
+        ----------
+        path : link
+            Đường dẫn tới nơi lưu trữ file word embedding trên local.
+        vector_dimension : integer
+            Số chiều của vecto.
+
+        Returns
+        -------
+        None.
+
+        """
         self.path = path 
         self.vector_dimension = vector_dimension
     
     @staticmethod
     # Chuyển đầu vào thành một mảng
     def get_coefs(word, *arr): 
+        """
+
+        Parameters
+        ----------
+        word : string
+        *arr : string
+            Tham số thể hiện độ ánh xạ ngữ nghĩa.
+
+        Returns
+        -------
+        word :từ
+        numpy
+
+        """
         return word, np.asarray(arr, dtype='float32')
     # Sử dụng dụng file word embedding có sẵn tạo thành từ điển
     def get_embedding_index(self):
+        """
+        
+
+        Returns
+        -------
+        embeddings_index : dict
+            Tạo thành từ điển word - tham số ánh xạ.
+
+        """
         embeddings_index = dict(self.get_coefs(*o.split(" ")) for o in open(self.path, errors='ignore'))
         return embeddings_index
     # Tạo ma trận
     def create_embedding_matrix(self, tokenizer, max_features):
         """
-        Hàm tạo ma trận embedding
+        
+
+        Parameters
+        ----------
+        tokenizer : class
+            class tách từ.
+        max_features : integer
+            số lượng word đầu vào.
+
+        Returns
+        -------
+        embedding_matrix : numpy
+            Ma trận embedding.
         """
         model_embed = self.get_embedding_index()
 
--- dataset/load_w2v_model.py
+++ dataset/load_w2v_model.py
@@ -2,10 +2,35 @@ from urllib.request import urlopen
 from zipfile import ZipFile
 
 class Load_w2v_model():
+    def __doc__(self):
+        return """
+    Input: Url word embedding model
+    Output: file word embedding có sẵn
+    Tính năng: download file word embedding ở định dạng zip và giải nén.
+    """
     def __init__(self,url='http://nlp.stanford.edu/data/glove.42B.300d.zip'):
+        """
+        Parameters
+        ----------
+        url : link http
+            Đ/c file word embedding online. The default is 'http://nlp.stanford.edu/data/glove.42B.300d.zip'.
+
+        Returns
+        -------
+        None.
+
+        """
         self.url = url
     
     def load_w2v(self):
+        """
+        Input: self.url
+
+        Returns
+        -------
+        file word embedding đã được giải nén.
+
+        """
         zipresp = urlopen(self.url)
         if (zipresp is not None):
             tempzip = open("C:/Users/Admin/Documents/GitHub/NLP/data/glove.zip", "wb")
