66e619e32369134641f5e86c670845d2403e05bf
Vietanh239 <72076528+Vietanh239@users.noreply.github.com>
2021-05-25 01:23:47 +0700

    Update classification_evaluate.py
    Co-Authored-By: quyenn2410 <65557160+quyenn2410@users.noreply.github.com>
    Co-Authored-By: thientam332 <76957177+thientam332@users.noreply.github.com>

--- evaluate/classification_evaluate.py
+++ evaluate/classification_evaluate.py
@@ -1,11 +1,42 @@
 import dataset.generating_input as generator
 class Test():
+    """
+    Danh gia model 
+    """
     def __init__(self,model=None,X=None,Y=None,tokenizer=None):
+        """
+        
+
+        Parameters
+        ----------
+        model : class model, optional
+            Model cần được đánh giá . The default is None.
+        X : Các thành phần của bộ dữ liệu , optional
+             The default is None.
+        Y : Các label của bộ dữ liệu , optional
+            DESCRIPTION. The default is None.
+        tokenizer : class , optional
+            class chứa các tách từ . The default is None.
+
+        Returns
+        -------
+        None.
+
+        """
         self.model=model 
         self.X=X
         self.Y=Y
         self.tokenizer=tokenizer
     def predict_testdata(self):
+        """
+        
+        
+        Returns
+        -------
+        score : các giá trị của model được đánh giá 
+            độ chính xác của dữ liệu.
+
+        """
         TextToTensor_instance = generator.TextToTensor(tokenizer=self.tokenizer,max_len=20)
         self.X = TextToTensor_instance.string_to_tensor(self.X)
         score = self.model.evaluate(self.X, self.Y,verbose=0)
