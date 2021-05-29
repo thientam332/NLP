4e44e88eb13aa8a9cbb6cef6686399f5bd699cc6
thientam332 <76957177+thientam332@users.noreply.github.com>
2021-05-29 18:10:35 +0700

    somethings

--- evaluate_visual/evaluate_visual.py
+++ evaluate_visual/evaluate_visual.py
@@ -1,7 +1,33 @@
 from evaluate_visual.visual import visual
 from evaluate_visual.evaluate import classification_evaluate as evaluate
 class Evaluate_visual():
+    """
+    Lớp trực quan hóa và đánh giá kết quả
+    """
     def __init__(self,model=None,tokenizer=None,dataset=None,X=None,Y=None,History=None ):
+        """
+        Nhận và khởi tạo các dữ liệu để trực quan hóa và đánh giá mô hình.
+
+        Parameters
+        ----------
+        model : Object
+            model đã train. The default is None.
+        tokenizer : Object
+            Class được khởi tạo từ thư viện Tokenizer của keras. Dùng để xử lý Testdata trước khi dùng để đánh giá. The default is None.
+        dataset : DataFrame 
+            Bộ dữ liệu của dataset đã lưu trong project . The default is None.
+        X : list
+            Danh sách các bình luận phim. The default is None.
+        Y : list
+            Danh sách các nhãn tương ứng của bình luận. The default is None.
+        History : Object
+            Class lưu trữ lịch sử của model đã train. The default is None.
+
+        Returns
+        -------
+        None.
+
+        """
         self.model=model
         self.tokenizer=tokenizer
         self.dataset=dataset
@@ -9,13 +35,37 @@ class Evaluate_visual():
         self.Y=Y
         self.History=History
     def Evaluate(self):
+        """
+        Hàm đánh giá độ chính xác trên datatest
+
+        Returns
+        -------
+        dict
+             Gồm 2 giá trị loss và acc.
+        """
         review_movie_model_pre=evaluate.Test(self.model,self.X,self.Y,self.tokenizer)
         return review_movie_model_pre.predict_testdata()
     def Visuallize(self):
+        """
+        Hàm trực quan hóa dữ liệu ban đầu và kết quả dự đoán trên tập test với acc và val_acc
+
+        Returns
+        -------
+        None.
+
+        """
         visualize=visual.Visualize(self.dataset,self.X,self.History)
         visualize.VisualizeData()
         visualize.VisualizePredictModel2()
     def Visuallize1(self):
+        """
+        Hàm trực quan hóa kết quả dự đoán trên tập test với loss và val_loss
+
+        Returns
+        -------
+        None.
+
+        """
         visualize1=visual.Visualize(self.dataset,self.X,self.History)
         visualize1.VisualizePredictModel()  
     
--- models/models.py
+++ models/models.py
@@ -8,10 +8,36 @@ Created on Thu May 27 22:00:14 2021
 
 from models.lstm import layers, rnn, sentence_model, token_model
 class ModelLSTM():
+    """
+    Lớp xây dựng mô hình LSTM và train mô hình
+    """
     def __init__(self,Pre_train=None):
+        """
+        Nhận dữ liệu train mô hình
+
+        Parameters
+        ----------
+        Pre_train : object
+            Class chứa các tham số đầu vào đã được xử lý. The default is None.
+        self.model: object 
+            Khởi tạo đối tượng rỗng làm nơi lưu trữ cho quá trình train mô hình. The default is None.
+        Returns
+        -------
+        None.
+
+        """
         self.Pre_train=Pre_train
         self.model=None
     def CreateNode(self):
+        """
+        Xây dựng mô hình LSTM và train mô hình với các tham số từ Pre_train và các layer Keras
+
+        Returns
+        -------
+        Object
+            Lớp lịch sử của train model.
+
+        """
         self.model = layers.Sequential()
         embedding_layer = sentence_model.Embedding(self.Pre_train.get_embedding().shape[0], 300, weights=[self.Pre_train.get_embedding()], input_length=self.Pre_train.get_max_len() , trainable=False)
         self.model.add(embedding_layer)
@@ -20,4 +46,13 @@ class ModelLSTM():
         self.model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
         return self.model.fit(self.Pre_train.get_X_Train(), self.Pre_train.get_Y_Train(), batch_size=256, epochs=10,validation_split=0.2)
     def get_model(self):
+        """
+        Hàm trả về model đang được thực thi.
+
+        Returns
+        -------
+        Object
+            Model.
+
+        """
         return self.model
